2bab960cbae2a34f5728fbabbede880d2f85329a
alhalabiamro-cmd <alhalabiamro@gmail.com>
2026-05-20 13:12:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, send_file
 import requests
 import os
 
@@ -10,8 +10,12 @@ APIKEY = os.environ.get('APIKEY', 'c4c035341dbe1da1531b227d89f6e2f481252766')
 def cors(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, APIKEY'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, APIKEY, anthropic-version, x-api-key'
     return response
+
+@app.route('/bank-sync')
+def bank_sync():
+    return send_file('bank-sync.html')
 
 @app.route('/<path:endpoint>', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
 def proxy(endpoint):
@@ -34,7 +38,7 @@ def proxy(endpoint):
 
 @app.route('/')
 def health():
-    return cors(make_response(jsonify({'status': 'ok'}), 200))
+    return cors(make_response(jsonify({'status': 'ok', 'message': 'Daftra Proxy is running'}), 200))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
