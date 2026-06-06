c151f3fe9a9f10becb92e91f1469c7adb0fcd79a
alhalabiamro-cmd <alhalabiamro@gmail.com>
2026-06-06 15:53:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ def _load_job(job_id):
         return None
 
 CLIENT_ALIASES = {
+    'سلمان عبدالعزيز': 'سلمان العودة',
+    'سلمان العودة': 'سلمان العودة',
     'مهجة': 'ريميندر', 'reminder': 'ريميندر',
     'الخدمات التجارية المتكاملة': 'الخدمات التجارية المتكاملة',
     'خدمات متكاملة': 'الخدمات التجارية المتكاملة',
@@ -248,7 +250,7 @@ PERSONAL: عمرو الحلبي(2229429275), اميرة(2229429267)
 TRANSPORT: عبدالحسيب, عمرو بدوي, IBRAHIM
 LOCAL SUPPLIERS: السنا للرخام, الفرات للرخام, اسوار الخليج, هواهوي, جنى مارين, قمم الشام, بيتي النيق
 CHINA SUPPLIERS: GBOUEO02, SHENYANG, CNY transfers
-CLIENTS: ريميندر, مهجة, MISHARY ALZAMIL, SHARAF ALTALHI, هشام المسيند, نور البنعلى, اسامه العنزي, وليد الجحيش, سفيان الزامل, الخدمات التجارية المتكاملة, علي سعود, مؤسسة الجبر, شركة ذكي للدعاية, CAMBNI ALROMEH
+CLIENTS: ريميندر, مهجة, MISHARY ALZAMIL, SHARAF ALTALHI, هشام المسيند, نور البنعلى, اسامه العنزي, وليد الجحيش, سفيان الزامل, الخدمات التجارية المتكاملة, علي سعود, مؤسسة الجبر, شركة ذكي للدعاية, CAMBNI ALROMEH, سلمان العودة, سلمان عبدالعزيز
 OTHER: سليمان المهوس=rent, جي مارين=rent, LOANFLEET=loan, Mudud=salary, نقاط بيع=client_payment, بطاقة ائتمانية=bank_fee, قوس قزح=government, Ministry of Labor=government, Expatriate/Renew Iqama=government, SAUDI ELECTRIC/SEC/كهرباء=utilities, STC/زين/موبايلي/اتصالات=internet, STCPAY=internet, مياه=utilities, NWC=utilities, الاختيار الامثل/الختيار المثل/الاختيار المثل/تخليص جمركي/جمارك/customs clearance=manufacturing
 
 category values: client_payment | local_supplier | china_supplier | salary | rent | transportation | government | bank_fee | personal | loan | other
